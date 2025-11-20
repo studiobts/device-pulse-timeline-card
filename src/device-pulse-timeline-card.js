8ba9c321d2292cb2900ffc4707ff0fbafc87a8fb
studiobts <info@studiobts.it>
2025-11-20 18:35:16 +0000

    Refactor CSS loading

--- src/device-pulse-timeline-card.js
+++ src/device-pulse-timeline-card.js
@@ -104,7 +104,8 @@ class DevicePulseTimeline extends LitElement {
 
     async _loadCSS() {
         try {
-            const response = await fetch('/local/device-pulse-timeline/device-pulse-timeline.css');
+            const cssUrl = new URL('./device-pulse-timeline-card.css', import.meta.url);
+            const response = await fetch(cssUrl);
             const css = await response.text();
             const style = document.createElement('style');
             style.textContent = css;
